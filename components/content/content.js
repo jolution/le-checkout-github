f36e90df098251c4203d989e0e71922213d0d2d9
Julian Kasimir <julian.kasimir@eviden.com>
2023-09-25 11:10:08 +0200

    feat: add selectbox for branch dialog

--- components/content/content.js
+++ components/content/content.js
@@ -42,9 +42,6 @@ window.onload = function () {
     const prefixes = ['feature', 'fix', 'build', 'ci', 'docs', 'perf', 'refactor', 'style', 'test', 'chore', 'research'];
 
     const select = document.createElement('select');
-    select.addEventListener('change', () => {
-        updateBranchName(select.value);
-    });
 
     prefixes.forEach(prefix => {
         const option = document.createElement('option');
@@ -92,6 +89,10 @@ window.onload = function () {
             inputElement.value = `git checkout -b ${formattedBranchNameWithPrefix}`;
         }
 
+        select.addEventListener('change', () => {
+            updateBranchName(select.value);
+        });
+
         // Erstellen des "Copy"-Buttons
         const copyButton = document.createElement('button');
         copyButton.innerHTML = '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon d-inline-block"><path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path></svg>';
@@ -132,9 +133,14 @@ window.onload = function () {
 
                             const formInput = addedNode.querySelector('input[name="name"]');
                             const formInputValue = formInput.textContent || formInput.value;
+                            formInput.value = select[0].value + '/' + formInputValue;
 
                             // Add select field
                             insertAfter(select, formInput);
+                            select.addEventListener('change', () => {
+                                formInput.value = select.value + '/' + formInputValue;
+                            });
+
                         }
                     }
                 }
